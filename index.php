818a5e4699ea3844385f862baebec3a9c113f87f
Mykhailo <mishasokolov2709@gmail.com>
2024-09-24 10:38:35 +0200

    add simple router and false path handler

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
 
 require 'functions.php';
-
-$uri = $_SERVER['REQUEST_URI'];
-
-if ($uri === '/') {
-    require 'controllers/index.php';
-} else if ($uri === '/about') {
-    require 'controllers/about.php';
-} else if ($uri === '/contact') {
-    require 'controllers/contact.php';
-}
+require 'router.php';
