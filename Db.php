39b5b081888e5d87694f4f3d01c9b998c7b83893
Mykhailo <mishasokolov2709@gmail.com>
2024-09-26 09:11:39 +0200

    add PDO

---
+++ Db.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+
+    public $connection;
+
+    public function __construct() {
+        $dsn = "mysql:host=127.0.0.1;port=3306;dbname=myapp;charset=utf8mb4";
+
+        $this->connection = new PDO($dsn, 'root', '123465', []);
+    }
+
+    public function query($query) {
+
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+
+        return $statement;
+    }
+
+}
--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+require 'Db.php';
+
+$db = new Database();
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
